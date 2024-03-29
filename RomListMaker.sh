575b940ea8ded39ca1735eb55ff7fd613a22a1b8
Todd Duffin <1476397+TippyTurtle@users.noreply.github.com>
2024-03-29 01:03:31 -0700

    Update RomListMaker.sh
    Added DTD header and option to pull Pacman as a thing by itself.

--- RomListMaker.sh
+++ RomListMaker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 clear
 
-# xpath -q -e '/mame/machine[not (@cloneof) and not (@isbios="yes")]/driver[@status="good" or @status="imperfect"]//parent::machine/dipswitch/dipvalue[@name="Cocktail"]//parent::dipswitch//parent::machine//parent::mame' GameList.xml
-
 if ! command -v xpath &> /dev/null; then
     echo -e "\n\n\nThe 'xpath' command is not installed.\n\nMaybe try: sudo apt install libxml-xpath-perl\n\n"
     exit 1
@@ -23,28 +21,15 @@ else
     exit 1
 fi
 
-# Initialize a variable to store XML content
-xml_content=""
-
-# Read the XML file line by line
-while IFS= read -r line; do
-    # Append each line to the xml_content variable
-    xml_content+="$line"$'\n'
-    # Check if the line contains "<name>"
-    if [[ $line == *"<mame"* ]]; then
-        break  # Exit the loop after reaching "<name>"
-    fi
-done < "$1"
-
-RunCommand="xpath -q -e '/mame/machine"
+RunCommand="xpath -n -p '    ' -q -e '/mame/machine[not (@ismechanical=\"yes\")  and not (@runnable=\"no\")"
 OutputFileName="GameList-"
 
 echo
 read -p "Parent ROM's only? (Y/n) " -n 1 -r
 if [[ $REPLY =~ ^[Nn]$ ]]; then
-    RunCommand=$RunCommand"[not (@isbios=\"yes\")]/driver"
+    RunCommand=$RunCommand"]"
 else
-    RunCommand=$RunCommand"[not (@cloneof) and not (@isbios=\"yes\")]/driver"
+    RunCommand=$RunCommand" and not (@cloneof)]"
     OutputFileName=$OutputFileName"Parents"
 fi
 
@@ -53,22 +38,66 @@ read -p "Playable Rom's only? (Y/n) " -n 1 -r
 if [[ $REPLY =~ ^[Nn]$ ]]; then
     RunCommand=$RunCommand
 else
-    RunCommand=$RunCommand"[@status=\"good\" or @status=\"imperfect\"]" 
+    RunCommand=$RunCommand"/driver[@status=\"good\" or @status=\"imperfect\"]//parent::machine" 
     OutputFileName=$OutputFileName"Playable"
 fi
 
 echo
 read -p "Cocktail Table dipswitch Rom's only? (y/N) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    RunCommand=$RunCommand"//parent::machine/dipswitch/dipvalue[@name=\"Cocktail\"]//parent::dipswitch//parent::machine'"
-#    RunCommand=$RunCommand"//parent::machine/configuration/confsetting[@name=\"Cocktail\"]//parent::configuration//parent::machine'"
+    RunCommand=$RunCommand"/dipswitch/dipvalue[@name=\"Cocktail\"]//parent::dipswitch//parent::machine'"
+#    RunCommand=$RunCommand"/configuration/confsetting[@name=\"Cocktail\"]//parent::configuration//parent::machine'"
     OutputFileName=$OutputFileName"Cocktail"
 else
-    RunCommand=$RunCommand"//parent::machine'"
+    RunCommand=$RunCommand"'"
 fi
 
+echo
+read -p "Would you like to add the CLONE Pacman? (y/N) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    PacManMachine=""
+    echo -e "\n\nLooking for the Pacman Machine in $1. This might take a little while."
+    # Read the file line by line
+    while IFS= read -r line; do
+      # Check if the line contains "name="pacman""
+      if [[ $line == *"name=\"pacman\""* ]]; then
+        # Store the line in the PacManMachine variable
+        PacManMachine="$line"$'\n'
+        echo "Found the start of Pacman."
+        # Continue reading the file
+        while IFS= read -r line; do
+          # Add the line to the PacManMachine variable
+          PacManMachine+="$line"$'\n'
+          
+          # Check if the line contains "</machine>"
+          if [[ $line == *"</machine>"* ]]; then
+            echo "Found the end of Pacman."
+            # Exit the inner loop
+            break
+          fi
+        done
+        
+        # Exit the outer loop
+        break
+      fi
+    done < "$1"
+    OutputFileName=$OutputFileName"Pacman"
+fi
+
+# Copy the DTD and the mame tag with all of it attributes
+DTDHeader=""
+echo "Reading DTD header."
+while IFS= read -r line; do
+    # Append each line to the     DTDHeader variable
+        DTDHeader+="$line"$'\n'
+    # Check if the line contains "<name>"
+    if [[ $line == *"<mame"* ]]; then
+        break  # Exit the loop after reaching "<name>"
+    fi
+done < "$1"
+
 OutputFileName=$OutputFileName".xml"
-RunCommand="$RunCommand $1 >> $OutputFileName" 
+RunCommand="$RunCommand $1 >> $OutputFileName"
 
 echo
 if [ -e "$OutputFileName" ]; then
@@ -76,16 +105,19 @@ if [ -e "$OutputFileName" ]; then
     echo -e "**** $OutputFileName already exists. Delete it first then run this again. ****\n\n"
     echo
     echo "I was going to run (but didn't): $RunCommand"
-    echo -e "\n\n\n\n"
+    echo -e "\n\n"
     exit 1
 else
     echo -e "\n\n\n\n"
     echo "Running: $RunCommand"
     echo
     echo "Outputfile: $OutputFileName"
+    echo
+    echo "This will take a long time on a modern MAME gamelist.  It will take minutes, maybe dozens of minutes.  Press ctrl-c to bail out before it finishes."
     echo -e "\n\n\n\n"
-    echo "$xml_content" > $OutputFileName
+    echo "$DTDHeader" > $OutputFileName
     eval "$RunCommand"
+    echo "$PacManMachine" >> $OutputFileName
     echo "</mame>" >> $OutputFileName
     echo -e "\n\n\n\n"
 fi
